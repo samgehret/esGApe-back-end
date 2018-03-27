c0190f92ea9565ebf6051563f2397854e99abeab
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-03-27 12:15:47 -0400

    also, take note of the new directories I made so we don't somehow make duplicates.

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const jwt = require('jwt-simple')
-
+// more commit comments     
 const passport = require('../config/passport')
 const config = require('../config/config')
 
