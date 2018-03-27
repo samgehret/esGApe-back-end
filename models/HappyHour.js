08e4e1103a71f0d8e0acffd812bb57dd7b680316
Sam Gehret <sam.gehret@gmail.com>
2018-03-27 12:20:50 -0400

    added some more fields to the Lunch and Happy hour model. Added some more seed data

--- models/HappyHour.js
+++ models/HappyHour.js
@@ -2,7 +2,14 @@ const mongoose = require('../db/connection')
 
 const HappyHourSchema = new mongoose.Schema({
   name: String,
-  address: String
+  address: String,
+  website: String,
+  drinkPrice: String,
+  ambiance: String,
+  description: String,
+  crowds: String,
+  seating: String,
+  distance: String
 })
 
 const HappyHour = mongoose.model('HappyHour', HappyHourSchema)
--- models/LunchSpot.js
+++ models/LunchSpot.js
@@ -2,7 +2,16 @@ const mongoose = require('../db/connection')
 
 const LunchSpotSchema = new mongoose.Schema({
   name: String,
-  address: String
+  address: String,
+  website: String,
+  lunchPrice: Number,
+  recommendedItems: String,
+  foodType: String,
+  ambiance: String,
+  description: String,
+  crowds: String,
+  seating: String,
+  distance: String
 })
 
 const LunchSpot = mongoose.model('LunchSpot', LunchSpotSchema)
